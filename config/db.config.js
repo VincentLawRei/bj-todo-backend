5c385637f1853ae91eef237f642538d73f7244c3
VincentLawRei <sekainome@protonmail.com>
2023-05-14 22:36:04 +0300

    init

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     HOST: process.env.HOST,
-    USER: process.env.USER,
+    USER: process.env.USERNAME,
     PASSWORD: process.env.PASSWORD,
     DB: process.env.DB,
     dialect: "postgres",
